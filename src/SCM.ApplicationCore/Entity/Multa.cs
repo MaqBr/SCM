176e6857afefc18e940dd426d73b8cd0fbf2aaee
MARCIO ALEXANDRE DE QUEIROZ <maqw3@hotmail.com>
2018-11-28 21:00:43 -0300

    Add project files.

---
+++ src/SCM.ApplicationCore/Entity/Multa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCM.ApplicationCore.Entity
+{
+    public class Multa
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public double Pontuacao { get; set; }
+        //Propriedade de Navegação (Navigation Property)
+        public Veiculo Veiculo { get; set; }
+    }
+}
---
+++ src/SCM.ApplicationCore/Entity/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCM.ApplicationCore.Entity
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public string Renavam { get; set; }
+        //Propriedade de Navegação (Navigation Property)
+        public List<Multa> Multas { get; set; }
+    }
+}
